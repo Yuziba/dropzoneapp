4b1d2a62b11d46720d8770ec210ec35d576689ea
Yusuf Ziya BAYRAM <yusufzbayram@outlook.com>
2024-02-19 17:10:13 +0300

    gorunum duzenleme

--- dropzoneapp/views.py
+++ dropzoneapp/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+"""def index(request):
+    return HttpResponse("deneme")"""
+
 def index(request):
-    return HttpResponse("deneme")
+    return render(request, 'dropzoneapp/index.html')
